eb44809687ae3c3cacd586f308903e1cf1ecfc33
Joshy Cyriac <joshy.cyriac@netcetera.ch>
2012-04-16 16:46:58 +0200

    not finished yet, getting different result on FF and Chrome

--- test/tests.js
+++ test/tests.js
@@ -1,32 +1,71 @@
-// without input form is invalid
-test('evaluate global state', function() {
-  var validation_state;
-  $(document).on('global_validation_result', function(event, data) {
-    validation_state = data.state;
-    ok(true, 'global validation result triggered');
+function DefaultRemoteValidation(url) {
+  this.url = url;
+  this.validate = [function(field){return remoteValidation(field, url)}];
+}
+
+var remoteValidation = function(field, url) {
+  return $.ajax({
+    url: url,
+    data: $(field).attr('id')+"="+$(field).val()
   });
-  $.when($.fn.validate('evaluateGlobalState')).then(function() {
-    ok(true, 'evaluate globale state called');
-    equal(validation_state, 'invalid');
-  });
-});
+};
 
 //
 test('get field value from input', function() {
-  equal($.fn.validate('getFieldValue', $('#input_text')), 2);
+  expect(1);
+  equal($.fn.validate('getFieldValue', $('#input_text1')), 2, 'correctly read value from input field');
 });
 
-// test engine with remote validation
-test('validateInputField', function() {
+
+// evaluate global state test
+// input_text3 is invalid -> global state is invalid
+test('evaluate global state', function() {
+  expect(3);
+  
   $.mockjax({
-    url : 'validate/form1',
-    dataType : 'json',
-    responseText : {
-      state : 'valid',
-      msg : 'input is valid'
+    url: 'validate/form2/input_text3',
+    dataType: 'json',
+    responseText: {
+      state: 'invalid'
     }
   });
-  $.fn.validate('validateAll');
-  ok($('#input_text').nextAll('.msg').text === 'valid', 'text input is valid');
+  
+  $('#input_text3').validate(new DefaultRemoteValidation('validate/form2/input_text3'));
+  
+  $(document).on('global_validation_result', function(event, data) {
+    validation_state = data.state;
+    ok(true, 'global validation result triggered');
+    equal(data.state, 'invalid', 'global state is invalid');
+  });
+
+  $.when($.fn.validate('evaluateGlobalState')).then(function() {
+    ok(true, 'evaluate globale state called');
+  });
+  
+  $.mockjaxClear();
+});
+
+// testing the scope functionality
+test('scope test', function() {
+  expect(1);
+  
+  $.mockjax({
+    url: 'validate/form2/input_text2',
+    dataType: 'json',
+    responseText: {
+      state: 'valid'
+    }
+  });
+ 
+  $('#input_text2').validate(new DefaultRemoteValidation('validate/form2/input_text2'));
+
+  $('#input_text2').on('validation_valid', function(event, data){
+    equal(data.state, 'valid', 'input_text2 is valid');
+    start();
+  });
+  
+  $.fn.validate('validateAll', $('#form2-div1'));
+  stop();
+  
   $.mockjaxClear();
 });
